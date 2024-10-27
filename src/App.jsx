2ef8a9def97ad3e64284a6c428024a1fd3c7cfea
kazu-08 <syame.ken3@gmail.com>
2024-10-28 00:13:31 +0900

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-export default function App() {
-    return (
-      <>
-        <h1>Image gallery example</h1>
-        <div className="full-img">
-          <img
-            className="displayed-img"
-            src="images/pic1.jpg"
-            alt="Closeup of a human eye"
-          />
-          <div className="overlay"></div>
-          <button className="dark">Darken</button>
-        </div>
-        <div className="thumb-bar"></div>
-      </>
-    );
-  }
+import React ,{useState} from "react";
 
-  const obj = {
-    src:"images/pic1.jpg",alt:"Closeup of a human eye",
-    src:"images/pic2.jpg",alt:"Rock that looks like a wave",
-    src:"images/pic3.jpg",alt:"Purple and white pansies",
-    src:"images/pic4.jpg",alt:"	Section of wall from a pharoah's tomb",
-    src:"images/pic5.jpg",alt:"Large moth on a leaf"
-  };
+export default function App() {
+    const [selectImage,setSelectImage] = useState({
+        src:"/image/pic.jpg",
+        alt:"Closeup of a human eye"
+    })
+    const obj = [
+        { src: "/images/pic1.jpg", alt: "Closeup of a human eye" },
+        { src: "/images/pic2.jpg", alt: "Rock that looks like a wave" },
+        { src: "/images/pic3.jpg", alt: "Purple and white pansies" },
+        { src: "/images/pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+        { src: "/images/pic5.jpg", alt: "Large moth on a leaf" }
+    ];
+
+    const ImageClick = (image) =>{
+        setSelectImage(image);
+    }
+
+    return (
+        <>
+            <h1>Image gallery example</h1>
+            <div className="full-img">
+                <img
+                    className="displayed-img"
+                    src={selectImage.src}
+                    alt={selectImage.alt}
+                />
+                <div className="overlay"></div>
+                <button className="dark">Darken</button>
+            </div>
+            <div className="thumb-bar">
+                {obj.map((image, index) => (
+                    <img key={index} src={image.src} alt={image.alt} onClick={()=>ImageClick(image)}/>
+                ))}
+            </div>
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,5 @@
-import { createRoot } from "react-dom/client";
-	import App from "./App";
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App";
 
-	createRoot(document.querySelector("#content")).render(<App />);
-
-function Header() {
-    return <h1>hello</h1>;
-  }
-  
-  function App() {
-    const header = <Header />;
-    return <div>{header}</div>;
-  }
+ReactDom.createRoot(document.querySelector("#content")).render(<App />);
