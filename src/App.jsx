a0d3a8a8197ddb568d85e96bed1733135e5b20a7
kazu-08 <syame.ken3@gmail.com>
2024-10-28 00:18:26 +0900

    Appの修正

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ export default function App() {
         alt:"Closeup of a human eye"
     })
     const obj = [
-        { src: "/images/pic1.jpg", alt: "Closeup of a human eye" },
-        { src: "/images/pic2.jpg", alt: "Rock that looks like a wave" },
-        { src: "/images/pic3.jpg", alt: "Purple and white pansies" },
-        { src: "/images/pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
-        { src: "/images/pic5.jpg", alt: "Large moth on a leaf" }
+        { src: "./images/pic1.jpg", alt: "Closeup of a human eye" },
+        { src: "./images/pic2.jpg", alt: "Rock that looks like a wave" },
+        { src: "./images/pic3.jpg", alt: "Purple and white pansies" },
+        { src: "./images/pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+        { src: "./images/pic5.jpg", alt: "Large moth on a leaf" }
     ];
 
     const ImageClick = (image) =>{
